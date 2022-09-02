a7db165e68fc666c62b92ae21ea39a6b52afebae
SolarisBravo <elitkramer@gmail.com>
2022-09-01 20:32:31 -0700

    Fix GLTF importer crashing when run in parallel

--- Source/Plugins/GLTF/Loaders/GLTFLoader.cs
+++ Source/Plugins/GLTF/Loaders/GLTFLoader.cs
@@ -3,6 +3,7 @@ using Engine;
 using Engine.Resources;
 using Engine.Mathematics;
 using Assimp;
+using Assimp.Configs;
 using AI = Assimp;
 using ModelPart = Engine.Resources.ModelPart;
 using Material = Engine.Resources.Material;
@@ -14,9 +15,10 @@ namespace Basic.Loaders
 {
 	public class GLTFLoader : AssetLoader<Model>
 	{
-		static AssimpContext importContext = new AssimpContext();
-
 		public string Path;
+
+		[ThreadStatic]
+		private static AssimpContext importContext = new();
 
 		public GLTFLoader(string path)
 		{
@@ -25,8 +27,19 @@ namespace Basic.Loaders
 
 		public override async Task<Model> Load()
 		{
-			// Load GLTF file.
-			AI.Scene importScene = importContext.ImportFile(Path, PostProcessSteps.PreTransformVertices);
+			// Create import context if needed.
+			if (importContext == null)
+			{
+				importContext = new AssimpContext();
+
+				importContext.SetConfig(new AppScaleConfig(1));
+				importContext.SetConfig(new FavorSpeedConfig(true));
+			}
+
+			// Load GLTF file from thread-local context.
+			PostProcessSteps steps = PostProcessSteps.GenerateNormals | PostProcessSteps.GenerateUVCoords;
+			//steps |= PostProcessSteps.CalculateTangentSpace;
+			AI.Scene importScene = importContext.ImportFile(Path, steps);
 
 			// Create embedded materials.
 			Material[] materials = new Material[importScene.MaterialCount];
@@ -43,6 +56,8 @@ namespace Basic.Loaders
 			ConcurrentBag<Mesh> meshes = new();
 			Parallel.ForEach(importScene.Meshes, (importMesh, state) =>
 			{
+				Debug.Assert(importMesh.PrimitiveType == PrimitiveType.Triangle, "Engine does not support non-triangle geometry.");
+
 				Vector3[] vertices = new Vector3[importMesh.VertexCount];
 				Vector3[] normals = new Vector3[importMesh.VertexCount];
 
