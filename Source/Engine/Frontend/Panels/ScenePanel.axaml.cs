8a4c2ffcd68dea8063f486af505b30be014f24a1
SolarisBravo <elitkramer@gmail.com>
2022-09-15 13:46:15 -0700

    Move ScenePanel to XAML

---
+++ Source/Engine/Frontend/Panels/ScenePanel.axaml.cs
@@ -0,0 +1,26 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Engine.Editor;
+using Engine.World;
+
+namespace Engine.Frontend
+{
+	public partial class ScenePanel : ToolPanel
+	{
+		[Notify] public ReadOnlyObservableCollection<Actor> SceneActors => Scene.Main?.Actors;
+
+		public ScenePanel()
+		{
+			DataContext = this;
+			InitializeComponent();
+
+			// Subscribe to property changed notifications in case the scene changes (i.e. new project).
+			StaticNotify.Subscribe(typeof(Scene), nameof(Scene.Main), () => (this as INotify).Raise(nameof(SceneActors)));
+
+			// Bind TreeView to editor selection. TODO: Figure out how to move this to XAML.
+			sceneTree.Bind(TreeView.SelectedItemsProperty, new Binding("selected", BindingMode.Default) { Source = typeof(Selection) });
+		}
+	}
+}
